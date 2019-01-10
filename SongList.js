941b99805b18706736c459a66414a5559d5c2acb
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-01-10 15:22:20 -0700

    Update SongList.js

--- SongList.js
+++ SongList.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
-(NOTE: React Component Design)
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
 
 class SongList extends Component {
     render() {
-        return( <div>
+        return( 
+        <div>
             SongList
         </div>
         );
     }
 }
+
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
 
 export default SongList;
