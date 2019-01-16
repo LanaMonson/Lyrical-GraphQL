d40f981297a5f71b26ad8804366c4d12712fd686
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-01-16 09:09:59 -0700

    Update SongDetail.js

--- SongDetail.js
+++ SongDetail.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
 
 class SongDetail extends Component{
     render(){
+        const { song } = this.props.data;        
+
+        if (!song) { return <div>Loading...</div>; }//<- can remove "Loading..." later
+
         return (
             <div>
-                <h3>Song Detail</h3>
-            </div>
+                <Link to="/">Back</Link>
+                <h3>{ song.title }</h3>
+                <LyricCreate songId={ this.props.params.id } />
+            </div> //same songId as in LyricCreate.js file; child component.
         );
     }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+    options: (props) => { return {variables: { id: props.params.id } } }//<-fetchSong query
+})(SongDetail);//<- this pattern is reproducible to any project, you can use it to make a query
